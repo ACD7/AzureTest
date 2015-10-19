6ecdf393e08ec54e11ee0666434987f5b25a7c2f
1200974 <andrewduncan777@btinternet.com>
2015-10-19 14:32:17 +0100

    Lab 1 - Q2

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php
+                echo "Hello World";
+                ?>
+            </p>
+        </body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 1200974
+ * Date: 19/10/2015
+ * Time: 14:27
+ */
