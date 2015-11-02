0522ad33657213acc8af58e4c6d42e13b3d65e4c
1200974 <andrewduncan777@btinternet.com>
2015-11-02 15:32:37 +0000

    Creating Login Page 2

--- display.php
+++ display.php
@@ -4,7 +4,7 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 if($username == "Andrew" && $password == "jimfeeley1"){
-    echo "Welcome " . $username . " Your Login was Successful";
+    echo "Welcome " . $username . ", your Login was Successful";
 }
 else{
     echo "Login Failed";
