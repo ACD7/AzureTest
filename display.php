afb3944f86a66825d80221ab35d8cc2deba32f3b
1200974 <andrewduncan777@btinternet.com>
2015-11-02 15:37:18 +0000

    set 2 cookies

--- display.php
+++ display.php
@@ -5,6 +5,8 @@ $password = $_POST["password"];
 
 if($username == "Andrew" && $password == "jimfeeley1"){
     echo "Welcome " . $username . ", your Login was Successful";
+    setcookie('username', $username);
+    setcookie('access_level', 'admin');
 }
 else{
     echo "Login Failed";
