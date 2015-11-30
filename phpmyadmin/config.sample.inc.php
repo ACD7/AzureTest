b4fe095906797fc11d6179b1443098e5ca682294
1208195 <1208195@rgu.ac.uk>
2015-11-30 15:22:05 +0000

    #yolo

--- phpmyadmin/config.sample.inc.php
+++ phpmyadmin/config.sample.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = 'ba734hye36u19q5'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'jimfeeley123456'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
